cf83d47e2c1dbe67a38f5d1c08c9e0b3dc42151a
Paul <swiergoszp@gmail.com>
2018-06-19 11:45:43 -0600

    html and css updated with psudeo in the js doc

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+// 1. user takes quiz
+
+    // set up timer that starts at start button click
+
+        // insert start button that starts timer and removes class .d-none from question div
+        // insert 60 second timer in row with header
+        // add/remove classes to some objects to progress DOM appearance
+
+    // create submit button that ends game before timer and moves to next step
+    // or timer moves on if user hasnt submitted
+
+// 2. answers have specific value that assigns outcome
+
+    // store value from the answers in individual vars
+    // create var of total value of each individual var
+
+// 3. results display likelyhood of survival in the wastes and potential "top skill"
+
+    // using total var display answer under submit area that tells user if they
+    // are meant to wander out of the vault
+    // individual answers will tell the user if they have any particular skills of 
+    // value, such as affinity for guns, sneakability, etc.
+
